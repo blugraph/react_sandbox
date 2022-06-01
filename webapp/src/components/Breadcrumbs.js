0335bdac484713e113b50866d9fc834a2d3c1d40
Noor_jahan <noor@blugraph.com>
2022-06-01 20:51:33 +0800

    changes

--- webapp/src/components/Breadcrumbs.js
+++ webapp/src/components/Breadcrumbs.js
@@ -1,5 +1,4 @@
-import { BreadcrumbItem, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 
 export default function Breadcrumbs(props) {
   return (
@@ -8,14 +7,6 @@ export default function Breadcrumbs(props) {
         <div className="page-title-box d-sm-flex align-items-center justify-content-between">
           <h4 className="mb-0 font-size-18">{props.breadcrumbItem}</h4>
           <div className="page-title-right">
-            {/* <ol className="breadcrumb m-0">
-              <BreadcrumbItem>
-                <Link to="#">{props.title}</Link>
-              </BreadcrumbItem>
-              <BreadcrumbItem active>
-                <Link to="#">{props.breadcrumbItem}</Link>
-              </BreadcrumbItem>
-            </ol> */}
           </div>
         </div>
       </Col>
--- webapp/src/components/VerticalLayout/Sidebar.js
+++ webapp/src/components/VerticalLayout/Sidebar.js
@@ -1,5 +1,5 @@
 import { Nav, NavDropdown } from "react-bootstrap";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/images/logo_new.png";
 import logoBg from "../../assets/images/bg_full.jpeg";
 import { useToggle } from "../ToggleContext";
@@ -20,43 +20,20 @@ export default function Sidebar(props) {
         </Link>
       </div>
       <div id="sidebar-menu">
-        {/* <ul className="metismenu list-unstyled" id="side-menu">
-          <li>
-            <Link to="/#" className="">
-              <i className="fas fa-home"></i>
-              <span>Dashboards</span>
-            </Link>
-          </li>
-
-          <li>
-          <Link to="/#">
-            <i className="fas fa-people-arrows"></i>
-            <span>Projects</span></Link>
-          </li>
-          <li>
-          <Link to="/#">
-            <i className="fas fa-map-marked-alt"></i>
-            <span>Map</span></Link>
-          </li>
-          <li>
-          <Link to="/#">
-            <i className="fas fa-user-shield"></i>
-            <span>Administrator</span></Link>
-          </li>
-          <li></li>
-        </ul> */}
-         <Nav activeKey="1" className="flex-column sidebarNavLink">
-          <Nav.Item style={{lineHeight:"3rem",paddingLeft:"0.9rem"}}> <Nav.Link onClick={()=>{user.setDateVal();navigate({pathname:'/dashboard'},{replace:true});}}> <i className="fas fa-home"></i> <span>Dashboards</span></Nav.Link></Nav.Item>
-          <Nav.Item style={{lineHeight:"3rem",paddingLeft:"0.9rem"}}> <Nav.Link onClick={()=>{user.setDateVal();navigate({pathname:'/projects'},{replace:true});}}> <i className="fas fa-people-arrows"></i><span>&nbsp;Projects </span></Nav.Link></Nav.Item>
-          <Nav.Item style={{lineHeight:"3rem",paddingLeft:"0.9rem"}}> <Nav.Link onClick={()=>{user.setDateVal();navigate({pathname:'/maps'},{replace:true});}}> <i className="fas fa-map-marked-alt"></i><span>&nbsp;Map</span> </Nav.Link></Nav.Item>
-          <Nav.Item style={{lineHeight:"3rem",paddingLeft:"0.9rem"}}> <Nav.Link onClick={()=>{user.setDateVal();navigate({pathname:'/settings'},{replace:true});}}> <i className="fas fa-cog"></i><span>&nbsp;Settings</span> </Nav.Link></Nav.Item>
-          <NavDropdown title={<><i className='fas fa-user-shield'></i><span>Administrator</span></> } id="nav-dropdown">
-            <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-         </Nav>
+        <Nav activeKey="1" className="flex-column sidebarNavLink">
+          <Nav.Item className="NavItemCls"> <Nav.Link onClick={() => { user.setDateVal(); navigate({ pathname: '/dashboard' }, { replace: true }); props.tToggle();}}> <i className="fas fa-home"></i> <span>Dashboards</span></Nav.Link></Nav.Item>
+          <Nav.Item className="NavItemCls"> <Nav.Link onClick={() => { user.setDateVal(); navigate({ pathname: '/projects' }, { replace: true }); props.tToggle();}}> <i className="fas fa-people-arrows"></i><span>&nbsp;Projects </span></Nav.Link></Nav.Item>
+          <Nav.Item className="NavItemCls"> <Nav.Link onClick={() => { user.setDateVal(); navigate({ pathname: '/maps' }, { replace: true }); props.tToggle();}}> <i className="fas fa-map-marked-alt"></i><span>&nbsp;Map</span> </Nav.Link></Nav.Item>
+          <Nav.Item className="NavItemCls"> <Nav.Link onClick={() => { user.setDateVal(); navigate({ pathname: '/settings' }, { replace: true }); props.tToggle();}}> <i className="fas fa-cog"></i><span>&nbsp;Settings</span> </Nav.Link></Nav.Item>
+          <Nav.Item className="NavItemCls">
+            <NavDropdown title={<><i className='fas fa-user-shield'></i><span>&nbsp;Administrator</span></>} id="nav-dropdown">
+              <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+              <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav.Item >
+        </Nav>
       </div>
     </div>
   );

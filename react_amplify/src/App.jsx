bbb2623edd539d10cb4275f187c56e8b8e4a9f1c
Jay <jay@blugraph.com>
2024-11-18 20:01:10 +0800

    Using v5

--- react_amplify/src/App.jsx
+++ react_amplify/src/App.jsx
@@ -1,14 +1,25 @@
+import "./assets/scss/App.scss";
+import '@aws-amplify/ui-react/styles.css';
+
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { BrowserRouter } from 'react-router-dom'
 import Layout from './Components/Layout/Layout'
-import "./assets/scss/App.scss";
-function App() {
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
+
+export default function App() {
   return (
-    <div className='App'>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
       <BrowserRouter>
         <Layout />
       </BrowserRouter>
-    </div>
-  )
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
 }
-
-export default App
